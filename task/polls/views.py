d01f61dad76055ceb197dafeceb9b270fc41758e
greatWeber <my_hero_666@sina.com>
2018-04-05 20:11:18 +0800

    添加登陆页面

--- task/polls/views.py
+++ task/polls/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+
 
 def index(request):
     return HttpResponse('task system')
+
+def login(request):
+    if request.method == 'POST':
+        return
+    else:
+        return render(request, 'polls/login.html')
